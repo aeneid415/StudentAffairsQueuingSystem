6f205214090b42ba6d9fddced0a409ea68ddd9f7
aeneid415 <aeneid415@hotmail.com>
2018-06-25 02:04:25 +0800

    Initial commit of project files

---
+++ TCPServerNew/TCPServerNew/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TCPServerNew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cubicleNumber = new System.Windows.Forms.Label();
+            this.servingNumber = new System.Windows.Forms.Label();
+            this.acceptConn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server has not yet started...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(509, 73);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cubicle Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(834, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(430, 73);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Now Serving: ";
+            // 
+            // cubicleNumber
+            // 
+            this.cubicleNumber.AutoEllipsis = true;
+            this.cubicleNumber.AutoSize = true;
+            this.cubicleNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 104.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cubicleNumber.Location = new System.Drawing.Point(262, 263);
+            this.cubicleNumber.Name = "cubicleNumber";
+            this.cubicleNumber.Size = new System.Drawing.Size(144, 159);
+            this.cubicleNumber.TabIndex = 3;
+            this.cubicleNumber.Text = "0";
+            // 
+            // servingNumber
+            // 
+            this.servingNumber.AutoSize = true;
+            this.servingNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 104.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.servingNumber.Location = new System.Drawing.Point(981, 263);
+            this.servingNumber.Name = "servingNumber";
+            this.servingNumber.Size = new System.Drawing.Size(144, 159);
+            this.servingNumber.TabIndex = 4;
+            this.servingNumber.Text = "0";
+            this.servingNumber.Click += new System.EventHandler(this.servingNumber_Click);
+            // 
+            // acceptConn
+            // 
+            this.acceptConn.Location = new System.Drawing.Point(1144, 673);
+            this.acceptConn.Name = "acceptConn";
+            this.acceptConn.Size = new System.Drawing.Size(172, 23);
+            this.acceptConn.TabIndex = 5;
+            this.acceptConn.Text = "Accept Incoming Connections";
+            this.acceptConn.UseVisualStyleBackColor = true;
+            this.acceptConn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(179, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 683);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.acceptConn);
+            this.Controls.Add(this.servingNumber);
+            this.Controls.Add(this.cubicleNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label servingNumber;
+        private System.Windows.Forms.Button acceptConn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.Label cubicleNumber;
+    }
+}
+
---
+++ TCPServerNew/TCPServerNew/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Media;
+
+
+namespace TCPServerNew
+{
+    public partial class Form1 : Form
+    {
+        TcpListener serverSocket = new TcpListener(IPAddress.Any, 8888);
+        TcpClient clientSocket = default(TcpClient);
+        List<TcpClient> listOfClients = new List<TcpClient>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            serverSocket.Start();
+            label1.Text = "Server Started";   
+
+
+
+            /*
+
+            Console.WriteLine(" >> " + "exit");
+
+            Console.ReadLine();
+            */
+            
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                String number = "";
+                if (serverSocket.Pending())
+                {
+
+                    clientSocket = serverSocket.AcceptTcpClient();
+                    listOfClients.Add(clientSocket);
+
+                    label6.Text = "Pending Connections";
+                    number = listOfClients.Count().ToString();
+
+
+                    label7.Text = "Number of Conn. Clients: " + number;
+
+                    //handleClinet client = new handleClinet();
+
+                    //client.startClient(clientSocket, Convert.ToString(counter));
+
+                    
+                }
+                else
+                {
+                    label6.Text = "No pending connections";
+                    number = listOfClients.Count().ToString();
+                    label7.Text = "Number of Conn. Clients: " + number;
+                }
+
+                if (listOfClients.Count() == 1)
+                {
+                    acceptConn.Visible = false;
+                    startClient(clientSocket);
+                    break;
+                }
+            }
+           
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                clientSocket.Close();
+                serverSocket.Stop();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+        }
+
+        public void startClient(TcpClient inClientSocket)
+        {
+            //this.clientSocket = inClientSocket;
+            //this.clNo = clineNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        delegate void SetTextCallback(string text);
+        delegate void SetServingCallback(string text);
+
+        private void SetText(string text)
+        {
+            if (this.cubicleNumber.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.cubicleNumber.Text = text;
+            }
+        }
+
+        private void SetServingText(String text)
+        {
+            int a = Convert.ToInt32(servingNumber.Text);
+            int b = Convert.ToInt32(text);
+            int c = a + b;
+            String newtext = Convert.ToString(c);
+            while (true)
+            {
+                
+                if (this.servingNumber.InvokeRequired)
+                {
+                    
+                    
+                    SetServingCallback d = new SetServingCallback(SetServingText);
+                    this.Invoke(d, new object[] { text });
+                    return;
+                }
+                else
+                {
+
+                    this.servingNumber.Text = newtext;
+                    break;
+                }
+            }
+        }
+
+        private void doChat()
+        {
+            //int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            //Byte[] sendBytes = null;
+            //string serverResponse = null;
+            //string rCount = null;
+            //requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+
+                    if (serverSocket.Pending())
+                    {
+
+                        clientSocket = serverSocket.AcceptTcpClient();
+                        listOfClients.Add(clientSocket);
+                    }
+                    //requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, 1);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    
+                    String newdataFromClient = dataFromClient.Substring(0, 1);
+                    playaudio();
+                    //Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
+                    if (newdataFromClient.Equals("0"))
+                    {
+                        cubicleNumber.Text = "0";
+                        servingNumber.Text = "0";
+                    }
+                    else
+                    {
+
+                        SetText(newdataFromClient);
+                        SetServingText("1");
+                        //cubicleNumber.Text = newdataFromClient;
+                        //servingNumber.Text = servingNumber.Text + 1;
+                        
+                    }
+                    
+                    
+
+
+                    /*rCount = Convert.ToString(requestCount);
+                    //serverResponse = "Server to clinet(" + clNo + ") " + rCount;
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                    */
+                }
+
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //MessageBox.Show("Please close the server before closing any of the clients.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //Application.Exit();
+                    continue;
+                }
+
+            }
+
+        }
+
+        private void servingNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playaudio() // defining the function
+        {
+            SoundPlayer audio = new SoundPlayer(TCPServerNew.Properties.Resources.ElectronicChime); // here WindowsFormsApplication1 is the namespace and Connect is the audio file name
+            audio.Play();
+        }
+    }
+
+    /*
+    public class handleClinet
+    {
+        TcpClient clientSocket;
+        string clNo;
+        public void startClient(TcpClient inClientSocket, string clineNo)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        
+        private IPAddress getLocalIPAddress()
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                return null;
+            }
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            return host
+                .AddressList
+                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+        }
+        
+
+        private void doChat()
+        {
+            //int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            //requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    //requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    //dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    //Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
+
+
+                    //rCount = Convert.ToString(requestCount);
+                    serverResponse = "Server to clinet(" + clNo + ") " + rCount;
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> " + ex.ToString() + " " + (int)clientSocket.ReceiveBufferSize);
+                    break;
+                }
+
+            }
+
+        }
+       
+
+    }
+    */
+}
